d833be3834787765ac69bdca6bb3b751a3e0a150
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-09-21 14:33:16 +0300

    first commit

---
+++ src/app/app1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({selector: 'appTEST1', templateUrl: './app1.component.html', styleUrls: ['./app.component.css']})
+export class App1Component
+{
+  skill_information: string = "Умею устанавливать винду 10 и хром";
+  knowledge_information: string = "Знаю, как скрафтить TNT в Майне";
+  certificates_information: string = "Подарочный сертификат центроптика бессрочный";
+  ava: string = "https://lh3.googleusercontent.com/proxy/vgzR-M_d6ATFTVNXrovW3SDHrhBLibrtBFqbkjgodqFoDqB6Xtkl6GkREapOdyuQpDGb1Cc7M7GSj4T-GMwYQz30T6oaKMGfgyBxrA";
+  link1: string = "https://github.com/sasuka2008?tab=repositories";
+  link2: string = "https://twitter.com/sasuka0202";
+}
